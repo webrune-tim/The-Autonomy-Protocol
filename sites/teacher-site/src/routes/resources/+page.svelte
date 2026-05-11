6d016e3e2eb748203114625f87c9dde15ed1cd07
Tim Smith <tim.smith.hdg@gmail.com>
2026-05-11 13:43:30 -0700

    Refactor sections for improved styling and margins

--- sites/teacher-site/src/routes/resources/+page.svelte
+++ sites/teacher-site/src/routes/resources/+page.svelte
@@ -30,7 +30,7 @@
 
 
 
-<section class="angled-top-bottom-box" style="--color: var(--brand-teal)">
+<section class="angled-top-bottom-box thick-margins" style="--color: var(--brand-teal)">
 	<h2>Twelve Steps</h2>
 	<p>
 		The Steps are a series of practical guides that walk you through the process of
@@ -53,7 +53,7 @@
 	{/if}
   </section>
 
-  <section class="angled-top-box no-bottom-margin" style="--color: var(--brand-orange)">
+  <section class="angled-top-box thick-margins no-bottom-margin" style="--color: var(--brand-orange)">
 	<h2>Five Aggrements</h2>
 	<p>
 		The Aggrements are a collection of agreements that you can use to govern yourself and
