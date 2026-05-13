08c1cce3c995306862c9caf257d0b558093384be
Tim Smith <tim.smith.hdg@gmail.com>
2026-05-13 09:07:44 -0700

    Implement redirect after logout POST request
    Add redirect after sign-out with correct status code.

--- sites/teacher-site/src/routes/logout/+server.ts
+++ sites/teacher-site/src/routes/logout/+server.ts
@@ -4,5 +4,7 @@ import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async ({ request }) => {
     await auth.api.signOut({ headers: request.headers });
+    // 303 See Other is the correct status code for redirecting after a POST
     throw redirect(303, "/");
 };
+
