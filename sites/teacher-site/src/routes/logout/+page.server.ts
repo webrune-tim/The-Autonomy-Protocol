a2436962b94fb5eb0c7fdb2d2d323c40974cfa48
Tim Smith <tim.smith.hdg@gmail.com>
2026-05-13 07:25:21 -0700

    Add logout logic with redirection
    Implement logout functionality to redirect users if logged in.

--- sites/teacher-site/src/routes/logout/+page.server.ts
+++ sites/teacher-site/src/routes/logout/+page.server.ts
@@ -1,1 +1,15 @@
 // If user is logged in, log them out
+
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+import type { PageServerLoad } from "./$types";
+import { auth } from "$lib/server/auth";
+import { APIError } from "better-auth/api";
+
+export const load: PageServerLoad = (event) => {
+  const redirectTo = event.url.searchParams.get("redirectTo") || "/dashboard";
+  if (event.locals.user) {
+    return redirect(302, redirectTo);
+  }
+  return {};
+};
