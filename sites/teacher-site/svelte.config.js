a4f260c409809970708acf6f5a0a7af2cc28277e
Tim Smith <tim.smith.hdg@gmail.com>
2026-06-12 13:10:49 -0700

    Mdsvex Fix

--- sites/teacher-site/svelte.config.js
+++ sites/teacher-site/svelte.config.js
@@ -1,31 +1,41 @@
-import { mdsvex } from 'mdsvex'
-import adapter from '@sveltejs/adapter-vercel'
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) =>
-			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-	},
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$actions: 'src/lib/actions',
-			$components: 'src/lib/components',
-			$docs: 'src/lib/docs',
-			$images: 'src/lib/assets/images',
-			$stores: 'src/lib/stores'
-		},
-		typescript: {
-			config: (config) => ({
-				...config,
-				include: [...config.include, '../drizzle.config.ts']
-			})
-		}
-	},
-	preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
-	extensions: ['.svelte', '.svx', '.md']
-}
+  compilerOptions: {
+    // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+    runes: ({ filename }) => (filename.split(/[/\\]/).includes("node_modules") ? undefined : true),
+  },
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $actions: "src/lib/actions",
+      $components: "src/lib/components",
+      $docs: "src/lib/docs",
+      $images: "src/lib/assets/images",
+      $stores: "src/lib/stores",
+    },
+    typescript: {
+      config: (config) => ({
+        ...config,
+        include: [...config.include, "../drizzle.config.ts"],
+      }),
+    },
+  },
+  preprocess: [
+    mdsvex({ extensions: [".svx", ".md"] }),
+    {
+      markup({ content, filename }) {
+        if (filename && (filename.endsWith(".md") || filename.endsWith(".svx"))) {
+          return {
+            code: content.replace(/<script\s+context="module">/g, "<script module>"),
+          };
+        }
+      },
+    },
+  ],
+  extensions: [".svelte", ".svx", ".md"],
+};
 
-export default config
+export default config;
