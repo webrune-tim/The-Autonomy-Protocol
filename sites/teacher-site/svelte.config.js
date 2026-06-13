f171646fac081f222d6e05493c9e24abace2e9d9
Tim Smith <tim.smith.hdg@gmail.com>
2026-06-12 20:24:47 -0700

    Update Image Adapter Configuration

--- sites/teacher-site/svelte.config.js
+++ sites/teacher-site/svelte.config.js
@@ -8,7 +8,15 @@ const config = {
     runes: ({ filename }) => (filename.split(/[/\\]/).includes("node_modules") ? undefined : true),
   },
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      images: {
+        sizes: [640, 828, 1200, 1920, 3840],
+        formats: ["image/avif", "image/webp"],
+        minimumCacheTTL: 300,
+        domains: [],
+      },
+      external: ["@libsql/client", "bufferutil", "utf-8-validate"],
+    }),
     alias: {
       $actions: "src/lib/actions",
       $components: "src/lib/components",
